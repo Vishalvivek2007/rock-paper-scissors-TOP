54e3e29fba1fa1de049086d8e8bffb8362a99333
Vishal Vivek <vishal.vivek.10039@gmail.com>
2025-11-21 22:38:59 +0530

    Final changes to UI of game

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getComputerChoice(){
 }
 let scorecard=document.querySelector('.scorecard');
 let current=document.querySelector('.present-round');
+let final=document.querySelector('.Final');
 function playgame(hchoice){
     if(humanscore==5 || Computerscore==5){
         return;
@@ -22,9 +23,53 @@ function playgame(hchoice){
     else{
         let cchoice= getComputerChoice();
         if(hchoice=='rock'){
-            if(choice=='rock'){
-                
+            if(cchoice=='rock'){
+                current.innerText="You chose Rock and The Computer also chose Rock , Its a Tie";
             }
+            else if(cchoice=='paper'){
+                current.innerText="You chose Rock and The Computer chose paper, YOU LOST!!";
+                Computerscore++;
+
+            }
+            else if(cchoice =='scissors'){
+                current.innerText="You chose Rock and the Computer chose Scissors, You Won!!";
+                humanscore++;
+            }
+        }
+        else if(hchoice=='paper'){
+            if(cchoice=='rock'){
+                current.innerText="You chose Paper and The Computer chose Rock , You Won";
+                humanscore++;
+            }
+            else if(cchoice=='paper'){
+                current.innerText="You chose Paper and The Computer chose paper, It is a TIE!!";
+
+            }
+            else if(cchoice =='scissors'){
+                current.innerText="You chose Paper and the Computer chose Scissors, You Lost!!";
+                Computerscore++;
+            }
+        }
+        else {
+            if(cchoice=='rock'){
+                current.innerText="You chose Scissors and The Computer chose Rock , You Lost!!";
+                Computerscore++;
+            }
+            else if(cchoice=='paper'){
+                current.innerText="You chose Scissors and The Computer chose paper, You WIN!!";
+                humanscore++;
+
+            }
+            else if(cchoice =='scissors'){
+                current.innerText="You chose Scissors and the Computer chose Scissors, It is a TIE!!";
+            }
+        }
+        scorecard.innerText=`Your Score:-${humanscore} Computer Score:-${Computerscore}`;
+        if(humanscore==5){
+            final.innerText="You WON The game, refresh to play again";
+        }
+        else if(Computerscore==5){
+            final.innerText="Computer WON The game, refresh to play again";
         }
     }
 }
