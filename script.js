85c104c96bd2c1c386e301a6dbff2fb8b857d5dc
Vishal Vivek <vishal.vivek.10039@gmail.com>
2025-11-21 22:24:37 +0530

    UI changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanscore=0;
+let Computerscore=0;
 
 function getComputerChoice(){
     let choice = Math.random();
@@ -8,89 +10,22 @@ function getComputerChoice(){
         return "paper";
     }
     else{
-        return "Scissors";
-    }
-    
-}
-
-function getHumanChoice(){
-    let Humanchoice = prompt("Enter your choice (rock) (paper) (scissors): ");
-    Humanchoice=Humanchoice.toLowerCase();
-    if(Humanchoice==="rock"){
-        return "rock";
-    }
-    else if(Humanchoice==="paper"){
-        return "paper";
-    }
-    else if(Humanchoice==="scissors"){
         return "scissors";
-    }
+    }   
 }
-
-function WON(score){
-    console.log("You WON the Round!!");
-    return score+1;
-}
-
-function LOST(score){
-    console.log("You LOST the Round!! ");
-    return score+1;
-}
-function TIE(){
-    console.log("The ROUND TIED play again!! ");
-}
-function playRound() {
-    let human=0;
-    let computer =0;
-    let round=1;
-    while(human!=5 && computer!=5){
-        console.log(`<---------- Round${round} ----------> `);
-        Humanchoice = getHumanChoice();
-        ComputerChoice = getComputerChoice();
-        console.log(`You chose: ${Humanchoice} and Computer Choice: ${ComputerChoice}`);
-
-        if(Humanchoice==="rock"){
-            if(ComputerChoice==="rock"){
-                TIE();
-            }
-            else if(ComputerChoice==="paper"){
-                computer=LOST(computer);
-            }
-            else {
-                human= WON(human);
-            }
-        }
-        else if(Humanchoice==="paper"){
-            if(ComputerChoice==="rock"){
-                computer=LOST(computer);
-            }
-            else if(ComputerChoice==="paper"){
-                TIE();
-            }
-            else {
-                human= WON(human);
-            }
-        }
-        else if(Humanchoice==="scissors"){
-            if(ComputerChoice==="rock"){
-                computer=LOST(computer);
-            }
-            else if(ComputerChoice==="paper"){
-                human= WON(human);
-            }
-            else {
-                TIE();
-            }
-        }
-        round++;
-    }
-    if(computer==5){
-        console.log("<-----------You LOST the game , computer won 5 rounds !!------->");
+let scorecard=document.querySelector('.scorecard');
+let current=document.querySelector('.present-round');
+function playgame(hchoice){
+    if(humanscore==5 || Computerscore==5){
+        return;
     }
     else{
-        console.log("<--------You WON the game , you won 5 rounds !!--------->");
+        let cchoice= getComputerChoice();
+        if(hchoice=='rock'){
+            if(choice=='rock'){
+                
+            }
+        }
     }
 }
-
-playRound();
 
